a6a4e65f997ab6fdf41b818ecacee58803796f43
Jonathan Chan <johnkchan252@gmail.com>
2018-08-31 01:30:26 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -19,12 +19,7 @@ module FormsLab
         Ship.new(details)
       end
       @ships = Ship.all
-      
-<<<<<<< HEAD
       erb :'pirates/show'
-=======
-      erb :'pirates/new'
->>>>>>> 702fbcb8c12335008a00deca1b3c984672d0a85c
     end
     
   end
